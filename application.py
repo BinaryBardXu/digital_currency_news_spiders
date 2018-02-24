25b6b9f9881db6336dd3bff930205d559245f547
Tao <bitao.ux@gmail.com>
2018-02-24 11:31:11 +0800

    Write pid when application starting.

--- application.py
+++ application.py
@@ -3,11 +3,12 @@
 import os
 from app import scheduler_runner
 from config import profiles
+from app.utils import pid
 
 active_profile = os.getenv('profile') or 'default'
 
 config = profiles[active_profile]
 
-
 if __name__ == '__main__':
+    pid.write()
     scheduler_runner.run()
