4a5aa9baba28291660317ba762bb243f62d87db0
Tao <bitao.ux@gmail.com>
2018-02-26 11:28:26 +0800

    Optimize NONGO_URL

--- app/repository/mongo.py
+++ app/repository/mongo.py
@@ -1,19 +1,19 @@
 from pymongo import MongoClient
 import application
 
-MONGO_URL = 'mongodb://%s:%s@%s:%s/%s?authSource=%s' % (application.config.MONGO_USER,
-                                                        application.config.MONGO_PASSWORD,
-                                                        application.config.MONGO_HOST,
-                                                        application.config.MONGO_HOST_PORT,
-                                                        application.config.MONGO_COLLECTION_NAME,
-                                                        application.config.MONGO_AUTH_SOURCE)
+MONGO_URL = 'mongodb://%s:%s@%s:%s/%s?authSource=%s&authMode=scram-sha1' % (application.config.MONGO_USER,
+                                                                            application.config.MONGO_PASSWORD,
+                                                                            application.config.MONGO_HOST,
+                                                                            application.config.MONGO_HOST_PORT,
+                                                                            application.config.MONGO_COLLECTION_NAME,
+                                                                            application.config.MONGO_AUTH_SOURCE)
 client = MongoClient(MONGO_URL)
 
 db = client.digital_currency_news
-collection = db.articles
+articles_collection = db.articles
 
 
 def save(article):
-    if collection.find({"$or": [{"link": article['link']}, {"title": article['title']}]}).count() > 0:
+    if articles_collection.find({"$or": [{"link": article['link']}, {"title": article['title']}]}).count() > 0:
         return
-    collection.insert_one(article)
+    articles_collection.insert_one(article)
