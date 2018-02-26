527db0c5334a2c4c5725485fab98919c74929879
Tao <bitao.ux@gmail.com>
2018-02-26 12:01:04 +0800

    Revert MONGO_URL

--- app/repository/mongo.py
+++ app/repository/mongo.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 import application
 
-MONGO_URL = 'mongodb://%s:%s@%s:%s/%s?authSource=%s&authMode=scram-sha1' % (application.config.MONGO_USER,
-                                                                            application.config.MONGO_PASSWORD,
-                                                                            application.config.MONGO_HOST,
-                                                                            application.config.MONGO_HOST_PORT,
-                                                                            application.config.MONGO_COLLECTION_NAME,
-                                                                            application.config.MONGO_AUTH_SOURCE)
+MONGO_URL = 'mongodb://%s:%s@%s:%s/%s?authSource=%s' % (application.config.MONGO_USER,
+                                                        application.config.MONGO_PASSWORD,
+                                                        application.config.MONGO_HOST,
+                                                        application.config.MONGO_HOST_PORT,
+                                                        application.config.MONGO_COLLECTION_NAME,
+                                                        application.config.MONGO_AUTH_SOURCE)
 client = MongoClient(MONGO_URL)
 
 db = client.digital_currency_news
