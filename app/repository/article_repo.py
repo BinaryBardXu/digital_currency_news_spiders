60375b04ecd6eccc161ad22abf42648267a58fce
Tao <bitao.ux@gmail.com>
2018-03-02 21:54:00 +0800

    Fix flake8 issue.

--- app/repository/article_repo.py
+++ app/repository/article_repo.py
@@ -7,14 +7,14 @@ articles_collection = 'articles'
 
 
 def save(article):
-    if mongo_client.collection(articles_collection).find(
-            {"$or":
-                [
-                    {"link": article['link']},
-                    {"title": article['title']}
-                ]
-            }).count() > 0:
+    article_count = mongo_client.collection(articles_collection).find({"$or": [
+        {"link": article['link']},
+        {"title": article['title']}
+    ]}).count()
+
+    if article_count > 0:
         return
+
     mongo_client.collection(articles_collection).insert_one(article)
     logger.info('已入库新的数据')
     logger.info(article)
