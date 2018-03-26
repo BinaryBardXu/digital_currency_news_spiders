4a06af03e9dbe0eb8c51f53f42dc01621bd28f21
Tao <bitao.ux@gmail.com>
2018-03-26 11:22:34 +0800

    Optimize jingji21 spider.

--- app/spiders/jingji21_spider.py
+++ app/spiders/jingji21_spider.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup
+import logging
 
 import requests
-from app.article import Article
+from bs4 import BeautifulSoup
+from requests import ConnectionError
 
 import config
+from app.article import Article
 from app.spiders.spider import Spider
 
 site_name = '21世纪经济报道'
 homePageUrl = 'http://www.21jingji.com/channel/finance/'
+logger = logging.getLogger(__name__)
 
 
 class Jingji21Spider(Spider):
@@ -26,9 +29,12 @@ class Jingji21Spider(Spider):
 
 
 def load_news():
-    home_page_data = requests.get(homePageUrl)
-    home_page_data.encoding = 'utf-8'  # 显式地指定网页编码，一般情况可以不用
+    try:
+        home_page_data = requests.get(homePageUrl)
+        home_page_data.encoding = 'utf-8'  # 显式地指定网页编码，一般情况可以不用
 
-    home_page_html = BeautifulSoup(home_page_data.text, "html.parser")
-    news_elements = home_page_html.find(class_="contList")
-    return news_elements.find_all('a', href=True)
+        home_page_html = BeautifulSoup(home_page_data.text, "html.parser")
+        news_elements = home_page_html.find(class_="contList")
+        return news_elements.find_all('a', href=True)
+    except ConnectionError:
+        logger.error('Connection timed out.' + homePageUrl)
